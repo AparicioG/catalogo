9d63ec3372867e2c7688b373d0793f67fb7ff4b4
AparicioG <4juana@gmail.com>
2017-02-24 08:27:50 -0600

    catalogo

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+/*$servidor="localhost";
+$usuario="root";
+$pwd="";
+$BD="catalogo";
+$conn=mysqli_connect($servidor,$usuario,$pwd,$BD) or die("Error al conectar la BD".mysqli_error());
+*/
+$servidor="mysql.hostinger.mx";
+$usuario="u839543735_jua1";
+$pwd="";
+$BD="u839543735_jua";
+$conn=mysqli_connect($servidor,$usuario,$pwd,$BD) or die("Error al conectar la BD".mysqli_error());
+
+?>
